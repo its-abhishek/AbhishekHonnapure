cace77c6b7c496aaddb1d431ee20c9043f6484c1
Abhishek Honnapure <100339461+its-abhishek@users.noreply.github.com>
2022-10-27 03:17:04 +0530

    uodated repo

---
+++ react-portfolio-website/src/components/header/CTA.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+import CTA from '../../assets/cv.pdf'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+      <a href= {CV} Download>Download CV</a>
+      <a href="#contact">Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
--- react-portfolio-website/src/components/header/header.jsx
+++ react-portfolio-website/src/components/header/header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import CTA from './CTA'
 import "./header.css"
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container header_container">
+      <h5>Hello I'm</h5>
+      <h1>Abhishek Honnapure</h1>
+      <h5 className="text-align">Fullstack Developer</h5>
+      <CTA />
+      </div>
+    </header>
   )
 }
 

e5115c3a0935b4b3dd4b9655ef89e0e5ca0a7799
Abhishek Honnapure <100339461+its-abhishek@users.noreply.github.com>
2022-10-28 01:26:04 +0530

    updated pst jsx files

--- react-portfolio-website/src/components/portfolio/portfolio.jsx
+++ react-portfolio-website/src/components/portfolio/portfolio.jsx
@@ -3,9 +3,9 @@ import "./portfolio.css"
 
 const Portfolio = () => {
   return (
-    <div>
+    <section id='portfolio'>
       Porfolio
-    </div>
+    </section>
   )
 }
 
--- react-portfolio-website/src/components/testimonials/testimonials.jsx
+++ react-portfolio-website/src/components/testimonials/testimonials.jsx
@@ -3,9 +3,9 @@ import "./testimonials.css"
 
 const Testimonials = () => {
   return (
-    <div>
+    <section id='testimonials'>
       Testimonials
-    </div>
+    </section>
   )
 }
 

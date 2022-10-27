7868f20bb2e1f29b642ca06a9d40c3e15e14720e
Abhishek Honnapure <100339461+its-abhishek@users.noreply.github.com>
2022-10-27 20:43:02 +0530

    updated Header file structure

--- react-portfolio-website/src/components/header/header.jsx
+++ react-portfolio-website/src/components/header/header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CTA from './CTA'
 import "./header.css"
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
       <h1>Abhishek Honnapure</h1>
       <h5 className="text-light">Fullstack Developer</h5>
       <CTA />
+      <HeaderSocials/>
       <div className="me">
         <img src={ME}alt="me" />
       </div>

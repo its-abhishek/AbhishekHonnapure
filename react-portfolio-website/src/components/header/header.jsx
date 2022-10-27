c9435ff3a0fc6855257406702fb3532d29ceee69
Abhishek Honnapure <100339461+its-abhishek@users.noreply.github.com>
2022-10-27 18:41:35 +0530

    changed classname of header

--- react-portfolio-website/src/components/header/header.jsx
+++ react-portfolio-website/src/components/header/header.jsx
@@ -6,7 +6,7 @@ import ME from '../../assets/me.png'
 const Header = () => {
   return (
     <header>
-      <div className="container header_container">
+      <div className="container header__container">
       <h5>Hello I'm</h5>
       <h1>Abhishek Honnapure</h1>
       <h5 className="text-light">Fullstack Developer</h5>
@@ -14,7 +14,7 @@ const Header = () => {
       <div className="me">
         <img src={ME}alt="me" />
       </div>
-        <a href="#contact" className='scroll_down'>Scroll Down</a>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )

3447406bab82f886179df84453dcf9b03008fa15
Abhishek Honnapure <100339461+its-abhishek@users.noreply.github.com>
2022-10-28 01:25:34 +0530

    updated nav css and jsx file

--- react-portfolio-website/src/components/nav/Nav.jsx
+++ react-portfolio-website/src/components/nav/Nav.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import "./nav.css"
-
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import { useState } from 'react'
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>
-      Nav
-    </div>
+    <nav>
+      <a href='#' className={ setActiveNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={ setActiveNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href='#experience'><BiBook/></a>
+      <a href='#services'><RiServiceLine/></a>
+      <a href='#contact'><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 

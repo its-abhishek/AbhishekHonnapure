3cfce76c15cdbdc0f2fff565f240995a123b91fc
Abhishek Honnapure <100339461+its-abhishek@users.noreply.github.com>
2022-11-01 15:27:17 +0530

    started services page - Abhishek

--- react-portfolio-website/src/components/services/services.jsx
+++ react-portfolio-website/src/components/services/services.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
     <section id='services'>
-      Services
+      <h5>What I Offer</h5> 
+      <h2>Services</h2>
+
+      <div className='conainer services_container'>
+        <article className='service'>
+            <div className='service_head'>
+              <h3>UI/UX Desing</h3>
+            </div>
+
+            <ul className='service_list'>
+              <li>
+                <BiCheck className='service_list-icon' />
+                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+              </li>
+            </ul>
+        </article>
+      </div>
     </section>
   )
 }

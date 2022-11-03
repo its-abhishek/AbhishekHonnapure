abe33fb2bfe0c606dac5527e76acdaca880dc2e4
Abhishek <abhihonnapure@gmail.com>
2022-11-03 20:29:01 +0530

    spelling mistake corrected - Abhishek

--- react-portfolio-website/src/components/services/services.jsx
+++ react-portfolio-website/src/components/services/services.jsx
@@ -8,7 +8,7 @@ const Services = () => {
       <h5>What I Offer</h5> 
       <h2>Services</h2>
 
-      <div className='conainer services__container'>
+      <div className='container services__container'>
         <article className='service'>
             <div className='service__head'>
               <h3>UI/UX Desing</h3>

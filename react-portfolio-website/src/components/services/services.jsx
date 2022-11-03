3defa6d4c2378ebd8cdee7382eb1b7338f31b533
Abhishek <abhihonnapure@gmail.com>
2022-11-03 20:39:25 +0530

    Added more article - Abhishek

--- react-portfolio-website/src/components/services/services.jsx
+++ react-portfolio-website/src/components/services/services.jsx
@@ -1,65 +1,164 @@
-import React from 'react'
-import './services.css'
-import {BiCheck} from 'react-icons/bi'
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <section id='services'>
-      <h5>What I Offer</h5> 
+    <section id="services">
+      <h5>What I Offer</h5>
       <h2>Services</h2>
 
-      <div className='container services__container'>
-        <article className='service'>
-            <div className='service__head'>
-              <h3>UI/UX Desing</h3>
-            </div>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Desing</h3>
+          </div>
 
-            <ul className='service__list'>
-              <li>
-                <BiCheck className='service__list-icon' />
-                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-              </li>
-              <li>
-                <BiCheck className='service__list-icon' />
-                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-              </li>
-              <li>
-                <BiCheck className='service__list-icon' />
-                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-              </li>
-              <li>
-                <BiCheck className='service__list-icon' />
-                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-              </li>
-              <li>
-                <BiCheck className='service__list-icon' />
-                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-              </li>
-              <li>
-                <BiCheck className='service__list-icon' />
-                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-              </li>
-              <li>
-                <BiCheck className='service__list-icon' />
-                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-              </li>
-              <li>
-                <BiCheck className='service__list-icon' />
-                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-              </li>
-              <li>
-                <BiCheck className='service__list-icon' />
-                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-              </li>
-              <li>
-                <BiCheck className='service__list-icon' />
-                <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-              </li>
-            </ul>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
         </article>
+        {/* end of UI/UX */}
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Desing</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of web-dev */}
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Desing</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* end of content curetion */}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;

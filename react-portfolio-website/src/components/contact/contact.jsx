a708332376ba53d419c0e770ad8bf7e31152fd80
Abhishek <abhihonnapure@gmail.com>
2022-11-05 23:36:44 +0530

    contact form done - Abhishek

--- react-portfolio-website/src/components/contact/contact.jsx
+++ react-portfolio-website/src/components/contact/contact.jsx
@@ -11,20 +11,20 @@ const Contact = () => {
 
       <div className='container contact__container'>
           <div className='contact__options'>
-            <article className='contact__options'>
-              <MdOutlineEmail/>
+            <article className='contact__option'>
+              <MdOutlineEmail className='contact__option-icon'/>
               <h5>Email</h5>
               <h5>abhihonnapure@gmail.com</h5>
               <a href='mailto:abhihonnapure@gmail.com' target='_blank'>Send a message</a>
             </article>
-            <article className='contact__options'>
-              <BsInstagram/>
+            <article className='contact__option'>
+              <BsInstagram className='contact__option-icon'/>
               <h5>Instagram</h5>
               <h5>@_its__abhi__7</h5>
               <a href='https://www.instagram.com/_its__abhi__7/' target='_blank'>Send a message</a>
             </article>
-            <article className='contact__options'>
-              <TfiTwitter/>
+            <article className='contact__option'>
+              <TfiTwitter className='contact__option-icon'/>
               <h5>Twitter</h5>
               <h5>@honnapure</h5>
               <a href='https://twitter.com/honnapure/following' target='_blank'>Send a message</a>
@@ -34,7 +34,7 @@ const Contact = () => {
           <form action=''>
             <input type='text' name='name' placeholder='Your Full Name' required/>
             <input type='email' name='email' placeholder='Your Email' required/>
-            <textarea type='message' row='7' placeholder='Your Massage' required></textarea>
+            <textarea type='message' rows='7' placeholder='Your Massage' required/>
             <button type='submit' className='btn btn-primary'>Send Massage</button>
           </form>
       </div>

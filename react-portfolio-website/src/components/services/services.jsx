73f4637067aa9a5bfd40eb94baf5382c5b09b187
Abhishek <abhihonnapure@gmail.com>
2022-11-06 02:53:49 +0530

    Added icons to header - Abhishek

--- react-portfolio-website/src/components/services/services.jsx
+++ react-portfolio-website/src/components/services/services.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./services.css";
 import { BiCheck } from "react-icons/bi";
+import { FaGraduationCap } from "react-icons/fa";
+import { MdOutlineWork } from "react-icons/md";
+import { MdVolunteerActivism } from "react-icons/md";
 
 const Services = () => {
   return (
     <section id="services">
-      <h5>What I Offer</h5>
-      <h2>Services</h2>
+      <h5>My Qualifications & Experience</h5>
+      <h2>Experience</h2>
 
       <div className="container services__container">
         <article className="service">
           <div className="service__head">
-            <h3>UI/UX Desing</h3>
+            <h3><MdOutlineWork className="service__head-icons"/>Work</h3>
           </div>
 
           <ul className="service__list">
@@ -40,7 +43,7 @@ const Services = () => {
         {/* end of UI/UX */}
         <article className="service">
           <div className="service__head">
-            <h3>Web-Development</h3>
+            <h3><FaGraduationCap className="service__head-icons"/>Education</h3>
           </div>
 
           <ul className="service__list">
@@ -77,7 +80,7 @@ const Services = () => {
         {/* end of web-dev */}
         <article className="service">
           <div className="service__head">
-            <h3>Content-Curetion</h3>
+            <h3><MdVolunteerActivism className="service__head-icons"/>Volunteering</h3>
           </div>
 
           <ul className="service__list">

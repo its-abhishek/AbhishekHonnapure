6a3d1d66ead7c0adcf771f62a34654131402f9ca
Abhishek <abhihonnapure@gmail.com>
2022-11-06 02:39:16 +0530

    added rel="noreferrer" - Abhishek

--- react-portfolio-website/src/components/header/HeaderSocials.jsx
+++ react-portfolio-website/src/components/header/HeaderSocials.jsx
@@ -5,9 +5,9 @@ import {SiTwitter} from 'react-icons/si'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://www.linkedin.com/in/abhishek-honnapure-547636232/" target="_blank"><BsLinkedin/></a>
-      <a href="https://github.com/its-abhishek" target="_blank"><FaGithub/></a>
-      <a href="https://twitter.com/honnapure" target="_blank"><SiTwitter/></a>
+      <a href="https://www.linkedin.com/in/abhishek-honnapure-547636232/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+      <a href="https://github.com/its-abhishek" target="_blank" rel="noreferrer"><FaGithub/></a>
+      <a href="https://twitter.com/honnapure" target="_blank" rel="noreferrer"><SiTwitter/></a>
     </div>
   )
 }

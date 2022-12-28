7df7f8b3c3118fdd3370fb7cf5b3ba33a9fdbf8f
abhihonnapure <abhihonnapure@gmail.com>
2022-12-28 06:13:08 +0530

    edited the service section - Abhishek

--- react-portfolio-website/src/components/services/services.jsx
+++ react-portfolio-website/src/components/services/services.jsx
@@ -4,6 +4,7 @@ import { BiCheck } from "react-icons/bi";
 import { FaGraduationCap } from "react-icons/fa";
 import { MdOutlineWork } from "react-icons/md";
 import { MdVolunteerActivism } from "react-icons/md";
+import { BiCalendar } from "react-icons/bi";
 
 const Services = () => {
   return (
@@ -18,30 +19,18 @@ const Services = () => {
           </div>
 
           <ul className="service__list">
-            <li>
+          <li>
               <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+              <p> <span style={{fontWeight:"bold"}}>Ninja Entrepreneur</span> <br/>Coding Ninjas<br/><span style={{fontWeight:"bold"}}>Internship</span><br/><span style={{opacity: "0.7"}}><BiCalendar/> April 2022 - September 2022</span></p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+              <p> <span style={{fontWeight:"bold"}}>Campus Ambassador</span> <br/>Acmegrade<br/><span style={{fontWeight:"bold"}}>Internship</span><br/><span style={{opacity:"0.7"}}><BiCalendar/> Jully 2022 - September 2022</span></p>
             </li>
           </ul>
         </article>
         {/* end of UI/UX */}
-        <article className="service">
+        <article className="service" /*style={{width:"25rem"}}*/>
           <div className="service__head">
             <h3><FaGraduationCap className="service__head-icons"/>Education</h3>
           </div>
@@ -49,31 +38,19 @@ const Services = () => {
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+              <p> <span style={{fontWeight:"bold"}}>B.Tech, CSE </span><br/> PES University,<br/> Bangalore <br/><span style={{opacity: "0.7"}}><BiCalendar/> 2021 - Present</span> </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+              <p> <span style={{fontWeight:"bold"}}>Inter 1st and 2nd year</span> <br/>Sri Chaitanya Junior College, Hyderabad<br/><span style={{opacity: "0.7"}}><BiCalendar/> 2019 - 2021</span></p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+              <p> <span style={{fontWeight:"bold"}}>5th-10th Grad</span> <br/> Raveendra High School,<br/> Bidar<br/><span style={{opacity: "0.7"}}><BiCalendar/> 2012 - 2018</span></p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p> Lorem, ipsum dolor sit amet consectetur elit.</p>
+              <p> <span style={{fontWeight:"bold"}}>1st-4th Grad</span> <br/> Ekalavya Primary School,<br/> Bidar<br/><span style={{opacity: "0.7"}}><BiCalendar/> 2008 - 2012</span></p>
             </li>
           </ul>
         </article>

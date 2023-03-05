e0f0132dc8a7a692683920848dc712e39fcb8f17
abhihonnapure <abhihonnapure@gmail.com>
2023-03-05 20:06:47 +0530

    minor change in Portfolio.js - Abhishek

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,10 +4,6 @@ import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
-// import IMG5 from '../../assets/portfolio5.png'
-// import IMG6 from '../../assets/portfolio6.jpg'
-
-// Don't use the images in the production
 
 const data = [
   {

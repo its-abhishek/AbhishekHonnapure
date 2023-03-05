45d29ae0437b7a3b81a7c188d6cf4895037916ee
abhihonnapure <abhihonnapure@gmail.com>
2023-03-05 20:02:39 +0530

    minor change in Nav.jsx - Abhishek

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./nav.css"
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {BiBook} from 'react-icons/bi'
+import { AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
+import { BiBook, BiMessageSquareDetail} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
-import {BiMessageSquareDetail} from 'react-icons/bi'
-import { useState } from 'react'
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (

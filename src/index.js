1e254789e0d575c989edae38ea6343be810d1072
abhihonnapure <abhihonnapure@gmail.com>
2023-03-05 19:55:14 +0530

    minor change in index.js - Abhishek

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App/>,document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render (
+            <React.Fragment>
+                <App/>
+            </React.Fragment>
+);

6af9a968e7a26f81ed432002fa3765dfac2330cb
abhihonnapure <abhihonnapure@gmail.com>
2023-03-06 08:30:06 +0530

    renamed named files - Abhishek

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from './components/about/About'
 import Experience from './components/skills/Skills' 
 import Services from './components/education/Education' 
 import Portfolio from './components/portfolio/Portfolio'
-import Awards from './components/awards/Awards'
+import Awards from './components/events/Events'
 import Contact from './components/contact/Contact' 
 import Footer from './components/footer/Footer' 
 

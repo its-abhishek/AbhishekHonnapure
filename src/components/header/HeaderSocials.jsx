c774b5a52f4fd08c3cb2768fcb85bfecadefe209
Abhishek Honnapure <100339461+its-abhishek@users.noreply.github.com>
2023-09-02 23:33:32 +0530

    Update HeaderSocials.jsx - Abhishek

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -9,7 +9,7 @@ const HeaderSocials = () => {
       <a href="https://www.linkedin.com/in/abhishek-honnapure-547636232/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
       <a href="https://github.com/its-abhishek" target="_blank" rel="noreferrer"><FaGithub /></a>
       <a href="https://twitter.com/honnapure" target="_blank" rel="noreferrer"><SiTwitter /></a>
-      <a href="https://www.instagram.com/_its__abhi__7/" target="_blank" rel="noreferrer"><RiInstagramFill /></a>
+      <a href="https://www.instagram.com/abhishek_honnapure/" target="_blank" rel="noreferrer"><RiInstagramFill /></a>
     </div>
   )
 }
